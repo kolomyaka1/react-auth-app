f3c66ac54801391bfdd55a633251ec4c7d153c48
kolomyaka1 <66018338+kolomyaka1@users.noreply.github.com>
2022-04-15 00:40:36 +0300

    Подключение firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { HomePage } from 'pages/HomePage';
 import { LoginPage } from 'pages/LoginPage';
 import { RegisterPage } from 'pages/RegisterPage';
 
+
 function App() {
   return (
     <Routes>
